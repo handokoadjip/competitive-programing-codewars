1984b0982d5cc587ed1dca42c91c8731b4739334
handokoadjipangestu <handokoadjipangestu@gmail.com>
2021-04-26 15:33:38 +0700

    add kyu8 remove first and last character

---
+++ kyu8/remove first and last character/index.js
@@ -0,0 +1,6 @@
+const removeChar = str => str.substring(1, str.length -1);
+
+console.log(removeChar('eloquent'), 'loquen');
+console.log(removeChar('country'), 'ountr');
+console.log(removeChar('person'), 'erso');
+console.log(removeChar('place'), 'lac'); 
