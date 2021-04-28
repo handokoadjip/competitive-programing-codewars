69a0ac9a4aeef99fce190e09d584ee88baf8f781
handokoadjipangestu <handokoadjipangestu@gmail.com>
2021-04-28 10:13:57 +0700

    add kyu8 counting sheep

---
+++ kyu8/counting sheep/index.js
@@ -0,0 +1,10 @@
+const countSheeps = arrayOfSheep => arrayOfSheep.filter(Boolean).length;
+
+var array1 = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true ];
+  
+console.log(countSheeps(array1) == 17, "There are 17 sheeps in total")
