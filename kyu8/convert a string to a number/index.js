04eb802ea89a77ed2f1931e797b5b849e2751f9d
handokoadjipangestu <handokoadjipangestu@gmail.com>
2021-06-14 17:56:49 +0700

    add kyu8 convert a string to a number

---
+++ kyu8/convert a string to a number/index.js
@@ -0,0 +1,6 @@
+const stringToNumber = (str) => Number(str);
+
+console.log(stringToNumber("1234"),1234)
+console.log(stringToNumber("605"), 605)
+console.log(stringToNumber("1405"),1405)
+console.log(stringToNumber("-7"),  -7)
