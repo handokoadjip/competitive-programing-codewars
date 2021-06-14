b728a628992a3ab1cb11d52ed7154c95550163cd
handokoadjipangestu <handokoadjipangestu@gmail.com>
2021-06-14 17:53:20 +0700

    add kyu8 invert values

---
+++ kyu8/invert values/index.js
@@ -0,0 +1,7 @@
+const invert = array => array.map(arr => -arr);
+
+
+console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
+console.log(invert([]), []);
+console.log(invert([0]), [-0]);
