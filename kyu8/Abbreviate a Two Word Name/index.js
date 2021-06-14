4078daeca395b33ca6454f6d3245fc83003372b5
handokoadjipangestu <handokoadjipangestu@gmail.com>
2021-06-14 13:08:59 +0700

    kyu8 add Abbreviate a Two Word Name

---
+++ kyu8/Abbreviate a Two Word Name/index.js
@@ -0,0 +1,8 @@
+const abbrevName = (name) => name.split(' ').map(abbrev => abbrev.charAt(0).toUpperCase()).join('.');
+
+
+console.log(abbrevName("Sam Harris"), "S.H");
+console.log(abbrevName("Patrick Feenan"), "P.F");
+console.log(abbrevName("Evan Cole"), "E.C");
+console.log(abbrevName("P Favuzzi"), "P.F");
+console.log(abbrevName("David Mendieta"), "D.M");
